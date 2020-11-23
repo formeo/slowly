5f24c29cdea0eaeb6267d9ab90a1602e33cfe7c9
rgordienko <rgordienko@sdvor.com>
2020-11-23 22:21:06 +0500

    improve middleware

--- pkg/middleware/timeout.go
+++ pkg/middleware/timeout.go
@@ -1,28 +1,32 @@
 package middleware
 
 import (
-	"encoding/json"
-	"github.com/formeo/slowly/pkg/models"
+	"context"
 	"io"
 	"net/http"
+	"time"
 )
 
-func CheckTimeoutValue(h http.Handler) http.Handler {
+func CheckTimeoutValue(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var postTimeout models.PostTimeout
-		_ = json.NewDecoder(r.Body).Decode(&postTimeout)
-		if postTimeout.Timeout > 5000 {
-
+		done := make(chan bool)
+		ctx, cancelFunc := context.WithTimeout(r.Context(), 5*time.Second)
+		defer cancelFunc()
+		go func() {
+			next.ServeHTTP(w, r)
+			close(done)
+		}()
+		select {
+		case <-done:
+			return
+		case <-ctx.Done():
 			w.WriteHeader(http.StatusBadRequest)
 			w.Header().Set("Content-Type", "application/json")
 			_, err := io.WriteString(w, `{"error":"timeout too long"}`)
 			if err != nil {
 				panic(err)
 			}
-			return
-
 		}
-
-		h.ServeHTTP(w, r)
 	})
+
 }
